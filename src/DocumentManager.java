c101f7cf148d4cc71b98e26831013cb6c36e12a6
paul <paul.rosca00@e-uvt.ro>
2021-11-24 12:42:52 +0200

    Singleton update

--- src/DocumentManager.java
+++ src/DocumentManager.java
@@ -1,11 +1,14 @@
 public class DocumentManager {
-    private static DocumentManager instance = new DocumentManager();
+    private static DocumentManager instance = null;
     private Book book;
 
     private DocumentManager() {
     }
 
     public static DocumentManager getInstance() {
+        if (DocumentManager.instance == null) {
+            DocumentManager.instance = new DocumentManager();
+        }
         return DocumentManager.instance;
     }
 
