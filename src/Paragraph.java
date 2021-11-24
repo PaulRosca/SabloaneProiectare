a7e9a90abe55bc935876038115d804b96a771dd9
paul <paul.rosca00@e-uvt.ro>
2021-11-24 11:45:59 +0200

    AlignmentStrategy

--- src/Paragraph.java
+++ src/Paragraph.java
@@ -1,29 +1,38 @@
-public class Paragraph implements Element{
+public class Paragraph implements Element {
     private String text;
+    private AlignStrategy alignStrategy;
 
     public Paragraph(String text) {
         this.text = text;
     }
 
     public void print() {
-        System.out.println("Paragraph: " + this.text);
+        if (this.alignStrategy != null) {
+            this.alignStrategy.render(this.text);
+        } else {
+            System.out.println(this.text);
+        }
     }
 
     @Override
     public void add(Element e) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void remove(Element e) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public Element get(int i) {
         // TODO Auto-generated method stub
         return null;
+    }
+
+    public void setAlignStrategy(AlignStrategy alignStrategy) {
+        this.alignStrategy = alignStrategy;
     }
 }
